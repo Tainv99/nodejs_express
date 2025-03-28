1e2607e9c7b89c9902d25dcce48a8985ee1a662d
Tainv99 <hunterpirate99@gmail.com>
2025-03-28 10:15:31 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.get("/test", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
